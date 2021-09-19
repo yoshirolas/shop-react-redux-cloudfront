add7c8a9b1ab97b49ea2a416ba438933215a3d09
Alexey Otjukh <yoshirolas@tut.by>
2021-09-19 21:45:54 +0300

    fix: import endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://ujhvl08zyb.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://4ppcx8iozi.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://ujhvl08zyb.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
