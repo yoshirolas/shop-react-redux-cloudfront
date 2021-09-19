0947ca476b1d16f7e6dcc5758ecd5de8d3089cf6
Alexey Otjukh <yoshirolas@tut.by>
2021-09-19 14:55:58 +0300

    fix: api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://vhkz47km9c.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://ujhvl08zyb.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://vhkz47km9c.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://ujhvl08zyb.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
